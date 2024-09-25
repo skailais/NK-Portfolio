3da56ff23aa8b5f579b57ee6550dd92b11e3e515
Pavel Kulbachnyi <77022646+skailais@users.noreply.github.com>
2024-09-25 11:36:56 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+// Находим все якорные элементы
+const anchors = document.querySelectorAll(".target");
 
+let currentIndex = 0; // Начинаем с первого якоря
+
+window.addEventListener(
+  "wheel",
+  function (event) {
+    // Прокрутка вниз
+    if (event.deltaY > 0) {
+      if (currentIndex < anchors.length - 1) {
+        currentIndex++; // Увеличиваем индекс, переходим к следующему якорю
+      }
+    }
+
+    // Прокрутка вверх
+    if (event.deltaY < 0) {
+      if (currentIndex > 0) {
+        currentIndex--; // Уменьшаем индекс, переходим к предыдущему якорю
+      }
+    }
+
+    // Получаем координаты текущего якоря
+    const anchorPosition =
+      anchors[currentIndex].getBoundingClientRect().top + window.scrollY;
+
+    // Прокручиваем на 50 пикселей ниже якоря
+    window.scrollTo({
+      top: anchorPosition + 50, // Смещение на 50 пикселей ниже
+      behavior: "smooth", // Плавная прокрутка
+    });
+
+    // Останавливаем стандартную прокрутку
+    event.preventDefault();
+  },
+  { passive: false }
+);
